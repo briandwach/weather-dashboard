3cb76471f483ae98ce9e06051aefa73657f14b2d
livefromthelevel <brian.d.wach@gmail.com>
2024-01-30 00:24:11 -0600

    <Added successful form submit>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
-var citySearchEl = $('#citySearch');
-var searchHistoryEl = $('searchHistory');
-var currentWeatherEl= $('currentWeather');
-var cityRenderEl = $('cityRender');
-var dateRenderEl = $('dateRender');
-var forecastRenderEl = $('forecastRender');
+var citySearchEl = $('#citysearch');
+var searchHistoryEl = $('#searchhistory');
+var cityInputEl = $('#cityinput');
+var currentWeatherEl= $('#currentweather');
+var cityRenderEl = $('#cityrender');
+var dateRenderEl = $('#daterender');
+var forecastRenderEl = $('#forecastrender');
 
 
 var apiKey = 'f792e5dc60b51965b173fc62383ec644';
+
+function startWeatherSearch(event) {
+    event.preventDefault();
+    console.log(cityInputEl.val());
+    $('input[type="text"]').val('');
+};
+
+
+citySearchEl.on('submit', startWeatherSearch); 
